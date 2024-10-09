e34075dc8696c1304aeff3fce1821f6f324d5e5f
VK4041 <151887513+VK4041@users.noreply.github.com>
2024-10-10 00:41:25 +1100

    fix error 8

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -22,7 +22,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 # Attaches the container registry to the cluster.
 
 resource "azurerm_role_assignment" "role_assignment" {
-  principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
